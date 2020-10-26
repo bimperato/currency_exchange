43c6e05427d02180d3b6cdc7b82a8c6b7a002777
bimperato <70171741+bimperato@users.noreply.github.com>
2020-10-26 15:17:09 -0400

    Update currency.js

--- currency.js
+++ currency.js
@@ -1,12 +1,12 @@
-const select = document.querySelectorAll("select"); 
-const input = document.querySelectorAll("input"); 
-const apiUrl = "https://api.exchangeratesapi.io/latest"; 
+const select = document.querySelectorAll("select"); // select both of the <select> from html
+const input = document.querySelectorAll("input"); // select both of the <input> from html
+const apiUrl = "https://api.exchangeratesapi.io/latest"; // api link
 let html = "";
-var today = new Date(); 
-var date = today.getMonth(); 
+var today = new Date(); // date object
+var date = today.getMonth(); // gets month 0-11
 var time =
   today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(); 
-var newDate = getDate(date) + " " + today.getDate(); 
+var newDate = getDate(date) + " " + today.getDate(); // setting newDate equal to the result of getDate and adding the day 1-31
 
 function getDate(date) {
   var day;
@@ -53,20 +53,19 @@ function getDate(date) {
 
 var myTIme = milToStandard(time);
 function milToStandard(value) {
-  if (value.length == 8) {
-  
-    var hour = value.substring(0, 2); 
-    var minutes = value.substring(3, 5);
-    var ident = "AM"; 
+  if (value.length == 8) { // expected length for military time
+    var hour = value.substring(0, 2); // extracting hour
+    var minutes = value.substring(3, 5); // extracting minutes
+    var ident = "AM"; // initalize AM/PM ident
     if (hour == 12) {
-      ident = "PM";
+      ident = "PM";  //If hour is 12 then should set identifier to PM
     }
     if (hour == 0) {
-      hour = 12;
+      hour = 12; //If hour is 0 then set to 12 for standard time 12 AM
       ident = "AM";
     }
-    if (hour > 12) {
-      hour = hour - 12;
+    if (hour > 12) {  // If hour > 12, convert to standard format and set identifier to PM
+      hour = hour - 12; 
       ident = "PM";
     }
     return hour + ":" + minutes + " " + ident;
@@ -78,21 +77,21 @@ function milToStandard(value) {
 var totalTime = newDate + ", " + myTIme;
 document.getElementById("time").innerHTML = totalTime;
 
-async function currency() {
+async function currency() { // need to use async in order to use await
 
-  const res = await fetch(apiUrl); 
-  const data = await res.json(); 
-  const arrKeys = Object.keys(data.rates);
+  const res = await fetch(apiUrl);  // when calling fetch API, the browser sends a request to that url
+  // then, the URL API will read that request, run some code, and send back a response
+  const data = await res.json(); // json converst it to human readable format
+  const arrKeys = Object.keys(data.rates); // object.keys method returns an array of strings with the objects property names
+  const rates = data.rates; // all of the rates based on EURO
   
-  const rates = data.rates; 
-
-
+  // map will apply callback function to every element and put result into new array
   arrKeys.map((item) => {
     html += `<option value="${item}">${item}</option>`;
   });
 
   for (let i = 0; i < select.length; i++) {
-    select[i].innerHTML = html; 
+    select[i].innerHTML = html; // putting the new options from above inside the select html
   }
   function converter(i, k) {
     input[i].value =
@@ -103,7 +102,6 @@ async function currency() {
   input[0].addEventListener("keyup", function () {
     converter(1, 0);
   });
-
 
   input[1].addEventListener("keyup", () => converter(0, 1));
 }
